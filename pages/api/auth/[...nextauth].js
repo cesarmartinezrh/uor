cc0c1834da25f2b75866bac409bb1f31511b069b
cesarmartinezrh <cesarmartinezrh@hotmail.com>
2023-04-20 11:26:15 -0600

    Adding getAsesores api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,24 +1,24 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import axios from "axios";
-import FormData from "form-data";
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import axios from 'axios'
+import FormData from 'form-data'
 
 const authOptions = {
   session: {
-    strategy: "jwt",
+    strategy: 'jwt'
   },
   secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   jwt: {
     secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-    encryption: true,
+    encryption: true
   },
   // Configure one or more authentication providers
   site: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
   providers: [
     CredentialsProvider({
-      type: "credentials",
+      type: 'credentials',
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
+      name: 'Credentials',
       // `credentials` is used to generate a form on the sign in page.
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
@@ -26,43 +26,41 @@ const authOptions = {
       credentials: {},
       async authorize(credentials) {
         try {
-          const formData = new FormData();
-          for (const key in credentials) {
-            formData.append(key, credentials[key]);
-          }
-
-          const response = await fetch(process.env.NEXT_PUBLIC_USERS_API, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: formData,
-          });
-
-          const user = response.data;
-          if (user) {
-            return user;
+          const response = await axios.post(
+            process.env.NEXT_PUBLIC_USERS_API,
+            credentials,
+            {
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+              }
+            }
+          )
+          console.log('works')
+          const user = response.data
+          console.log(user)
+          if (response.status === 200 && user) {
+            return user
           } else {
-            return null;
+            return null
           }
         } catch (error) {
-          throw new Error(error);
+          throw new Error(error)
         }
-      },
-    }),
+      }
+    })
   ],
   callbacks: {
     async jwt({ token, user }) {
-      return { ...token, ...user };
+      return { ...token, ...user }
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
-      session.user = token;
-      return session;
-    },
+      session.user = token
+      return session
+    }
   },
   pages: {
-    signIn: "/auth/signin",
-  },
-};
-export default NextAuth(authOptions);
+    signIn: '/auth/signin'
+  }
+}
+export default NextAuth(authOptions)
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,85 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { getSession, useSession } from 'next-auth/react'
+import { getSession } from 'next-auth/react'
 
 import TextInput from '../components/textinput'
 import Layout from '../components/layout'
 import Select from '../components/select'
 import options from '../data/captec.json'
 
-export default function Home() {
+export default function Home({ sessionData }) {
   const router = useRouter()
-  /*  const { num_emp, puesto, nombre_completo } = sessionData.session.user.data */
-  const { data, status } = useSession()
-
-  const loading = status === 'loading'
+  const { num_emp, puesto, nombre_completo } = sessionData.session.user.data
 
   useEffect(() => {
-    if (status === 'unauthenticated') {
+    if (!sessionData) {
       router.replace('/auth/signin')
     }
-  }, [status, router])
+  }, [sessionData, router])
 
-  if (status === 'authenticated')
+  const [filter, setFilter] = useState({})
+
+  const [asesores, setAsesores] = useState([])
+  console.log(asesores)
+
+  const handleChange = (e) => {
+    const { value, name } = e.target
+    setFilter((prevState) => ({ ...prevState, [name]: value }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const objParams = `${new URLSearchParams(filter).toString()}`
+    console.log(objParams)
+
+    try {
+      const filteredData = await fetch(
+        `http://localhost:3000/api/getAsesores?${objParams}`
+      )
+      const response = await filteredData.json()
+      setAsesores(response)
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
+  if (sessionData)
     return (
-      <Layout>
+      <Layout title={'Inicio'}>
         <div className='w-full flex flex-col gap-2 p-4 items-center'>
-          <div>Bienvenido: {'Hola'}</div>
-          <form className='w-full grid grid-rows-1 gap-2'>
-            <TextInput placeholder={'Asesor Técnico'} name={'nombre'} />
-            <TextInput placeholder={'Razón Social'} name={'rs'} />
-            <TextInput placeholder={'Estado'} name={'estado'} />
-            <TextInput placeholder={'RFN'} name={'rfn'} />
+          <div>Bienvenido: {nombre_completo}</div>
+          <form
+            onSubmit={handleSubmit}
+            className='w-full grid grid-rows-1 gap-2'
+          >
+            <input
+              type='text'
+              onChange={handleChange}
+              placeholder={'Asesor Técnico'}
+              name='nombre'
+            />
+            <input
+              type='text'
+              onChange={handleChange}
+              placeholder={'Razón Social'}
+              name='rs'
+            />
+            <input
+              type='text'
+              onChange={handleChange}
+              placeholder={'Estado'}
+              name='estado'
+            />
+            <input
+              type='text'
+              onChange={handleChange}
+              placeholder={'RFN'}
+              name='rfn'
+            />
             <Select options={options} />
+            <button type='submit'>Send it!</button>
           </form>
+          {asesores ? JSON.stringify(asesores, null, 3) : <p>Sin resultados</p>}
         </div>
       </Layout>
     )
