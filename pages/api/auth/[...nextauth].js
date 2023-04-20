958234cfaa1b31d487f910106825597d76c6a990
Cesar Martinez <cesarmartinez@Cesars-MacBook-Air.local>
2023-04-19 20:51:51 -0600

    Moving some things

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,23 @@
-import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import axios from 'axios'
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import axios from "axios";
 
 const authOptions = {
   session: {
-    strategy: 'jwt'
+    strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+  jwt: {
+    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+    encryption: true,
+  },
   // Configure one or more authentication providers
-  site: process.env.PRODUCTION ? 'https://uor.cnf.gob.mx/' : 'localhost:3000',
+  site: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
   providers: [
     CredentialsProvider({
-      type: 'credentials',
+      type: "credentials",
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: 'Credentials',
+      name: "Credentials",
       // `credentials` is used to generate a form on the sign in page.
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
@@ -21,47 +25,46 @@ const authOptions = {
       credentials: {},
       async authorize(credentials, req) {
         try {
-          const { usuario, password } = credentials
-          const loginInfo = new URLSearchParams()
-          loginInfo.append('usuario', usuario)
-          loginInfo.append('password', password)
+          const { usuario, password } = credentials;
+          const loginInfo = new URLSearchParams();
+          loginInfo.append("usuario", usuario);
+          loginInfo.append("password", password);
 
           const response = await axios.post(
             process.env.NEXT_PUBLIC_USERS_API,
             loginInfo.toString(),
             {
               headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-              }
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
             }
-          )
-          console.log('works')
-          const user = response.data
-          console.log(user)
+          );
+          const user = response.data;
+          console.log(user);
           if (response.status === 200 && user) {
-            return user
+            return user;
           } else {
-            return null
+            return null;
           }
         } catch (error) {
-          console.error(error)
-          return null
+          console.error(error);
+          return null;
         }
-      }
-    })
+      },
+    }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-      return { ...token, ...user }
+      return { ...token, ...user };
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
-      session.user = token
-      return session
-    }
+      session.user = token;
+      return session;
+    },
   },
   pages: {
-    signIn: '/auth/signin'
-  }
-}
-export default NextAuth(authOptions)
+    signIn: "/auth/signin",
+  },
+};
+export default NextAuth(authOptions);
